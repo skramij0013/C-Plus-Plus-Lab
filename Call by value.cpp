fad742b7c43c7541f69a03171b231fe776a2a185
skramij0013 <skramij0013@gmail.com>
2026-09-08 23:01:39 +0530

    Add files via upload

---
+++ Call by value.cpp
@@ -0,0 +1,25 @@
+// Call by Value in C++
+
+#include <iostream>
+using namespace std;
+
+void swap(int x, int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+
+    cout << "After swap: a = " << x << " b = " << y << endl;
+}
+
+int main()
+{
+    int a = 40;
+    int b = 50;
+
+    cout << "Before swap: a = " << a << " b = " << b << endl;
+
+    swap(a, b);
+
+    return 0;
+}
