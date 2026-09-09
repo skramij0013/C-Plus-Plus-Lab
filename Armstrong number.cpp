ef7484320e4aa9449b10c5a5c41b20cf073a453d
skramij0013 <skramij0013@gmail.com>
2026-09-09 07:48:47 +0530

    Add files via upload

---
+++ Armstrong number.cpp
@@ -0,0 +1,28 @@
+// Armstrong Number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, r, sum = 0, temp;
+
+    cout << "Enter the Number = ";
+    cin >> n;
+
+    temp = n;
+
+    while (n > 0)
+    {
+        r = n % 10;
+        sum = sum + (r * r * r);
+        n = n / 10;
+    }
+
+    if (temp == sum)
+        cout << "Number is Armstrong.";
+    else
+        cout << "Number is not Armstrong.";
+
+    return 0;
+}
